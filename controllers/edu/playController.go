c5076fe4a9c537f24047b31e20ea14c5812a9216
gsp <1554680230@qq.com>
2019-08-13 13:24:39 +0800

    视频播放

--- controllers/edu/playController.go
+++ controllers/edu/playController.go
@@ -4,7 +4,6 @@ import (
 	"edu_api/controllers"
 	"github.com/ant0ine/go-json-rest/rest"
 	"edu_api/models"
-	"log"
 )
 
 /**
@@ -14,10 +13,9 @@ type PlayController struct {
 	controller controllers.Controller
 }
 
-func (play *PlayController) GetPlayList(w rest.ResponseWriter, r *rest.Request)  {
-	var playLists []models.Media
+func (play *PlayController) GetPlayList(w rest.ResponseWriter, r *rest.Request) {
+	var playLists models.Media
 
 	playLists, play.controller.Err = play.controller.BaseOrm.GetPlayList(r)
-
-	log.Printf("the playlist is:%v", playLists)
+	play.controller.JsonReturn(w, play.controller, "playLists", playLists)
 }
--- services/playService.go
+++ services/playService.go
@@ -4,10 +4,11 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"edu_api/models"
 	"strconv"
-	"log"
 )
 
-func (baseOrm *BaseOrm) GetPlayList(r *rest.Request) (playLists []models.Media, err error) {
+var num int64
+
+func (baseOrm *BaseOrm) GetPlayList(r *rest.Request) (playList models.Media, err error) {
 
 	id, err := strconv.Atoi(r.PathParam("id"))
 	lesionId, err := strconv.Atoi(r.PathParam("lesion_id"))
@@ -17,25 +18,60 @@ func (baseOrm *BaseOrm) GetPlayList(r *rest.Request) (playLists []models.Media, 
 
 	chapterList := make([]models.Chapter, 10)
 
-	first := make([]models.Chapter, 1)
+	first := models.Chapter{}
 
-	if err := baseOrm.GetDB().Table("h_edu_chapters").Where("course_id = ? and status = 'published'", id).Find(&chapterList).Error; err != nil {
-		return nil, err
+	if err := baseOrm.GetDB().Table("h_edu_chapters").Where("course_id = ? and status = 'published'", id).Find(&chapterList).Error; err != nil || len(chapterList) == 0 {
+		return playList, err
 	}
+
+	//当前课程类型
+	type courseType struct {
+		Type string `json:"type"`
+	}
+	cType := courseType{}
+
+	baseOrm.GetDB().Table("h_edu_chapters").Select("h_edu_courses.type").Joins("left join h_edu_courses on h_edu_courses.id = h_edu_chapters.course_id").Where("h_edu_chapters.id = ?", lesionId).Find(&cType)
 
 	if err := baseOrm.GetDB().Table("h_edu_chapters").Where("id = ? and status = 'published'", lesionId).Find(&first).Error; err != nil {
-		return nil, err
+		return playList, err
 	}
 
-	if len(first) == 0 {
+	if first.Id == 0 {
 		//就选择当前课程下最远的一条数据
-		if err := baseOrm.GetDB().Table("h_edu_chapters").Where("course_id = ? and status = 'published' and type = 'lesson'", id).Order("updated_at asc").Order("created_at asc").Find(&first).Error; err != nil {
-			return nil, err
+		if err := baseOrm.GetDB().Table("h_edu_chapters").Where("course_id = ? and status = 'published' and type = 'lesson'", id).Order("updated_at asc").Order("created_at asc").Find(&first).Error; err != nil || first.Id == 0 {
+			return playList, err
 		}
-
 	}
 
-	log.Printf("the playLists is:%v", first)
+	//数据组合
+	playList.Chapter = Trees(chapterList).([]models.Chapter)
+
+	if cType.Type == "class" {
+		//获取当前lesion所在的章节
+		row := baseOrm.GetDB().Table("h_edu_chapters").Select("title").Where("id = ?", recursion(baseOrm, lesionId)).Row()
+		row.Scan(&playList.CurrentTitle)
+	}
+
+	//根据类型判断
+	playList.CurrentLesion = first.Title
+	playList.LesionType = cType.Type
 
 	return
+}
+
+/**
+递归取父级
+ */
+func recursion(baseOrm *BaseOrm, parent_id int) (id int) {
+
+	row := baseOrm.GetDB().Raw("select parent_id from h_edu_chapters where id =?", parent_id).Row()
+	row.Scan(&id)
+
+	num++
+
+	if num == 3 {
+		return id
+	} else {
+		return recursion(baseOrm, id)
+	}
 }

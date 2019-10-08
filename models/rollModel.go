454cc3fb1249d895a7dcb4bcd5a469a1d71b2d31
guoshipeng <2249875980@qq.com>
2019-10-08 13:56:23 +0800

    channel处理答题状态

--- models/rollModel.go
+++ models/rollModel.go
@@ -4,18 +4,19 @@ package models
 题库模型
 */
 type RollModel struct {
-	Id              int64  `json:"id"`
-	Type            string `json:"type"`
-	Title           string `json:"title"`
-	Description     string `json:"description,omitempty"`
-	TotalScore      int64  `json:"total_score,omitempty"`
-	Mode            int    `json:"mode"`
-	ItemCount       int64  `json:"item_count"`
-	PassedCondition string `json:"passed_condition,omitempty"`
-	LimitedAt       int64  `json:"limited_at"`
-	Status          string `json:"status"`
-	CreatedAt       string `json:"created_at"`
-	UpdatedAt       string `json:"updated_at"`
-	CourseId        int64  `json:"course_id"`
-	ChapterId       int64  `json:"chapter_id,omitempty"`
+	Id              int64      `json:"id"`
+	Type            string     `json:"type"`
+	Title           string     `json:"title"`
+	Description     string     `json:"description,omitempty"`
+	TotalScore      int64      `json:"total_score,omitempty"`
+	Mode            int        `json:"mode"`
+	ItemCount       int64      `json:"item_count"`
+	PassedCondition string     `json:"passed_condition,omitempty"`
+	LimitedAt       int64      `json:"limited_at"`
+	Status          string     `json:"status"`
+	CreatedAt       string     `json:"created_at"`
+	UpdatedAt       string     `json:"updated_at"`
+	CourseId        int64      `json:"course_id"`
+	ChapterId       int64      `json:"chapter_id,omitempty"`
+	Grade           GradeModel `json:"grade"`
 }
--- services/examService.go
+++ services/examService.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"edu_api/models"
-	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
 	valid "github.com/asaskevich/govalidator"
 	log "github.com/sirupsen/logrus"
@@ -10,7 +9,11 @@ import (
 
 func (baseOrm *BaseOrm) GetExamRollTopicList(r *rest.Request) {
 	var (
-		rollList []models.RollModel
+		rollList   []models.RollModel
+		gradeChan  chan models.GradeModel
+		endChan    chan bool
+		endChanNum int
+		allChanNum int
 	)
 
 	id, err := valid.ToInt(r.PathParam("id"))
@@ -25,59 +28,50 @@ func (baseOrm *BaseOrm) GetExamRollTopicList(r *rest.Request) {
 
 	user = GetUserInfo(r.Header.Get("Authorization"))
 	//计算是否答过当前考卷，以及考试结果
-	test()
-
-}
-
-var (
-	msgs           string      //信息
-	msgChan        chan string //单条信息通道
-	endTaskNumChan chan bool   //完成任务通知
-	endTaskNum     int         //已完成任务数
-)
-
-func test() {
-	msgs = "初始值\n"
-	msgChan = make(chan string)
-	endTaskNumChan = make(chan bool)
-	endTaskNum = 0
-	for i := 0; i < 5; i++ {
-		go addMsg(i)
+	if len(rollList) == 0 {
+		return
 	}
+
+	allChanNum = len(rollList)
+	gradeChan = make(chan models.GradeModel, 4)
+	endChan = make(chan bool)
+	for _, value := range rollList {
+		go getGrade(baseOrm, value.Id, user.Id, gradeChan, endChan)
+	}
+
+	/**
+	  label处理channel
+	*/
 L:
-	//for  {
-	//	fmt.Println("msgs\n", msgs)
-	//	break L
-	//}
 	for {
 		select {
-		case msg, ok := <-msgChan: //获取单条信息
+		case grade, ok := <-gradeChan:
 			if ok {
-				msgs = fmt.Sprint(msgs, msg)
+
+				log.Printf("the grade is:%v", grade)
 			}
-		case <-endTaskNumChan: //获取处理完通知
-			endTaskNum++
-			if endTaskNum == 5 { //如果已完成任务等于总任务（退出这里是否有更好的办法？？）
-				close(msgChan)
+		case <-endChan:
+			endChanNum++
+			if endChanNum == allChanNum {
+				close(gradeChan)
+				close(endChan)
 				break L
 			}
 		}
-		fmt.Println("8888")
-	}
-	for i := 0; i < 10; i++ {
-		fmt.Println("10086")
 	}
 
-	fmt.Printf("msg is:%v", msgs)
-	fmt.Println("1")
-	fmt.Println("2")
-	fmt.Println("3")
 }
 
-func addMsg(i int) {
+/**
+获取当前题库成绩
+*/
+func getGrade(baseOrm *BaseOrm, rollId int64, userId int, gradeChan chan models.GradeModel, endChan chan bool) {
+	var grade models.GradeModel
+
 	defer func() {
-		endTaskNumChan <- true
+		endChan <- true
 	}()
-	//time.Sleep(1 * time.Second)
-	msgChan <- fmt.Sprint("当前是新邮箱：", i)
+
+	baseOrm.GetDB().Table("h_exam_grades").Where("roll_id = ? and user_id = ?", rollId, userId).First(&grade)
+	gradeChan <- grade
 }
